0f1385a2d1309c4e977b7898ba3263efb17c67fd
Daniel Schneider <daschne8@gmail.com>
2019-06-16 00:49:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,18 +16,40 @@ class Driver{
   }
 }
 let passengerId = 0
-class Passenger{
-  constructor(name){
-    this.name = name
-    this.id = ++passengerId
-    store.passengers.push(this)
+class Passenger {
+  constructor(name) {
+    this.name = name;
+    this.id = passengerId++;
+    store.passengers.push(this);
+  }
+
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.passengerId == this.id;
+    });
+  }
+  drivers() {
+    return this.trips().map(trip => {
+      return trip.driver();
+    });
   }
 }
 let tripId = 0
-class Trip{
-  constructor(name){
-    this.name = name
-    this.id = ++passengerId
-    store.trips.push(this)
+class Trip {
+  constructor(driver, passenger) {
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+    this.id = tripId++;
+    store.trips.push(this);
+  }
+  driver() {
+    return store.drivers.find(driver => {
+      return driver.id === this.driverId;
+    });
+  }
+  passenger() {
+    return store.passengers.find(passenger => {
+      return passenger.id === this.passengerId;
+    });
   }
 }
